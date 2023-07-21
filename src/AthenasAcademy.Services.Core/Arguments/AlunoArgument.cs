d32bc4e6caaa47e92003b61c91fdfc1ebc4917a9
rderoncio <rafael.deroncio@animaeducacao.com.br>
2023-07-21 00:28:35 -0300

    add arguments

---
+++ src/AthenasAcademy.Services.Core/Arguments/AlunoArgument.cs
@@ -0,0 +1,13 @@
+﻿namespace AthenasAcademy.Services.Core.Arguments;
+
+public class AlunoArgument
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public string Sobrenome { get; set; }
+    public char Sexo { get; set; }
+    public DateTime DataNascimento { get; set; }
+    public string Email { get; set; }
+    public DateTime DataInscricao { get; set; }
+    public bool Ativo { get; set; }
+}
