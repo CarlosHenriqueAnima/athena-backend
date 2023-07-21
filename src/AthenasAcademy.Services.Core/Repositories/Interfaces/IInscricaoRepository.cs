e0ec29192b9767ec1a42ffd053d90683ac2bc25a
hugo.damasceno <esmasiadamente@gmail.com>
2023-07-21 04:08:21 -0300

    Refazendo IInscricaoRepository após conflitos de merge

--- src/AthenasAcademy.Services.Core/Repositories/Interfaces/IInscricaoRepository.cs
+++ src/AthenasAcademy.Services.Core/Repositories/Interfaces/IInscricaoRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace AthenasAcademy.Services.Core.Repositories.Interfaces;
+﻿using AthenasAcademy.Services.Core.Models;
+
+namespace AthenasAcademy.Services.Core.Repositories.Interfaces;
 
 public interface IInscricaoRepository
 {
-
+    Task<CandidatoModel> ObterInscricaoPorIdAsync(int id);
+    Task<IEnumerable<CandidatoModel>> ObterTodasInscricoesAsync();
+    Task AdicionarInscricaoAsync(CandidatoModel inscricao);
+    Task AtualizarInscricaoAsync(CandidatoModel inscricao);
+    Task CancelarInscricaoAsync(int id);
 }
