f63b68fbcae98b05e273040c5727260b745808ec
rderoncio <rafael.deroncio@animaeducacao.com.br>
2023-07-20 23:31:49 -0300

    add database enum

---
+++ src/AthenasAcademy.Services.Core/Configurations/Enums/DatabaseEnum.cs
@@ -0,0 +1,12 @@
+﻿namespace AthenasAcademy.Services.Core.Configurations.Enums;
+
+public enum Database
+{
+    Usuario,
+    Inscricao,
+    Aluno,
+    Matricula,
+    Pagamento,
+    Curso,
+    Certificado,
+}
