38151280eee486d372e14fbc8cdb4143f9851d6b
rderoncio <rafael.deroncio@animaeducacao.com.br>
2023-07-21 00:03:20 -0300

    add models de inscricaoe matricula

---
+++ src/AthenasAcademy.Services.Core/Models/ContratoModel.cs
@@ -0,0 +1,13 @@
+﻿namespace AthenasAcademy.Services.Core.Models;
+
+public class ContratoModel
+{
+    public int Id { get; set; }
+    public string Contrato { get; set; }
+    public int DetalheContratoId { get; set; }
+    public bool Ativo { get; set; }
+    public DateTime DataCadastro { get; set; }
+    public DateTime? DataAlteracao { get; set; }
+
+    public DetalheContratoModel DetalheContrato { get; set; }
+}
