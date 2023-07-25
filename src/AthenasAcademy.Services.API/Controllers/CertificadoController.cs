a3415ddf7e0e75144832ac3f6228be654c8d4809
rderoncio <rafael.deroncio@animaeducacao.com.br>
2023-07-25 12:47:55 -0300

    ajuste controllers certificado

--- src/AthenasAcademy.Services.API/Controllers/CertificadoController.cs
+++ src/AthenasAcademy.Services.API/Controllers/CertificadoController.cs
@@ -31,12 +31,12 @@ public class CertificadoController : ControllerBase
     /// </summary>
     /// <param name="matricula">Número da matricula para o qual o certificado será gerado.</param>
     /// <returns>Retorna um IActionResult que contém o certificado gerado.</returns>
-    [HttpGet("gerar")]
-    //[Authorize(Roles = "Aluno, Admnistrador")]
-    //[ProducesResponseType(typeof(NovoCertificadoResponse), StatusCodes.Status201Created)]
-    //[ProducesResponseType(typeof(ExceptionResponse), StatusCodes.Status404NotFound)]
-    //[ProducesResponseType(typeof(ExceptionResponse), StatusCodes.Status500InternalServerError)]
-    public async Task<IActionResult> GerarCertificado()
+    [HttpGet("gerar/{matricula}")]
+    [Authorize(Roles = "Aluno, Admnistrador")]
+    [ProducesResponseType(typeof(NovoCertificadoResponse), StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(ExceptionResponse), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ExceptionResponse), StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> GerarCertificado(string matricula)
     {
         return Ok(await _certificadoService.GerarCertificado(string.Empty));
     }
@@ -47,7 +47,7 @@ public class CertificadoController : ControllerBase
     /// <param name="matricula">Número da matricula para o qual o certificado será obtido.</param>
     /// <returns>Retorna um IActionResult que contém o certificado em formato PDF.</returns>
     [HttpPost("obter/{matricula}")]
-    //[Authorize(Roles = "Aluno, Admnistrador")]
+    Authorize(Roles = "Aluno, Admnistrador")]
     [ProducesResponseType(typeof(ExceptionResponse), StatusCodes.Status404NotFound)]
     [ProducesResponseType(typeof(ExceptionResponse), StatusCodes.Status500InternalServerError)]
     public async Task<IActionResult> ObterCertificado(string matricula)
